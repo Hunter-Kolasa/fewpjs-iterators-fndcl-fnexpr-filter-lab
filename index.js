9a9e1fa64c56d7b25420b292f5f40ec1463250f9
Hunter <kolasa.hunter@aol.com>
2021-03-03 20:04:55 -0500

    commit tree 3dff6012ff7f421895f8ae59ee3c4a42bc8709e8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter(driver =>
+        driverName.toLowerCase() === driver.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, driverPartial) {
+    return drivers.filter( driver =>
+        driver.toLowerCase().indexOf(driverPartial.toLowerCase()) === 0
+    )
+}
+
+function matchName(driversObjectsArray, driverString) {
+    return driversObjectsArray.filter( driverObject => 
+       driverObject.name.toLowerCase() === driverString.toLowerCase() )
+}
